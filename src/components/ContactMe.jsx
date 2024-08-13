ee543378bd7ff5abac3549390e707a93da8dac28
Kawaljeet Singh <kawaljeet35@gmail.com>
2024-08-13 11:06:10 +0530

    Keys moved to .env

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,12 +1,16 @@
 import { contactData } from '../../data';
 import emailjs from 'emailjs-com';
 
+const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+
 export default function ContactMe() {
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_xn5k61f', 'template_ipdm1q9', e.target, 'SXJlNXQDroG490ir8')
+        emailjs.sendForm(serviceId, templateId, e.target, userId)
             .then((result) => {
                 console.log(result.text);
                 alert('Message sent successfully!');
